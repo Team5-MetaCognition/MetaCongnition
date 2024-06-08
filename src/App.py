34fd20a13faf05c2613624e13927a20047b18643
Y2NY <43701352+y2ny@users.noreply.github.com>
2024-06-09 00:08:45 +0900

    Feat: frame link (~단어 보여주는 페이지까지)

--- src/App.py
+++ src/App.py
@@ -1,11 +1,44 @@
-from tkinter import *
+import tkinter as tk
+from tkinter import PhotoImage
+from PIL import Image, ImageTk
+from UserInfo import UserInfo
+from StartPage import StartPage
+from HowToPage import HowToPage
+from ShowWord import ShowWord
 
-class App:
+# tk.Tk를 상속받아 창을 생성하고 관리
+class MetacognitionTestApp(tk.Tk):
     def __init__(self):
-        window = Tk()
-        window.title("메타인지 테스트")
-        window.geometry("1280x1024+100+100")
-    
-        window.mainloop()
+        super().__init__()
+        
+        self.title("메타인지 테스트")
+        self.attributes("-fullscreen", True)
+        self.fullScreenState = False
 
-App()
+        # 모든 Frame을 저장할 딕셔너리
+        self.frames = {}
+
+        container = tk.Frame(self)  # 주 창 안에 컨테이너 Frame을 생성
+        container.pack(fill="both", expand=True)  # 컨테이너를 화면에 맞게 확장
+
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        for Frame in (StartPage, HowToPage, UserInfo, ShowWord):  # 여러 페이지(Frame)를 초기화
+            page_name = Frame.__name__
+            frame = Frame(parent=container, controller=self)  # 각 Frame에 부모와 컨트롤러 전달
+            self.frames[page_name] = frame  # 딕셔너리에 Frame 저장
+            frame.grid(row=0, column=0, sticky="nsew")  # 모든 Frame을 같은 위치에 배치
+
+        self.swtich_frame("StartPage")  # 시작 페이지를 표시
+    
+    def swtich_frame(self, page_name):
+        frame = self.frames[page_name]  # 전달된 페이지 이름에 해당하는 Frame을 가져옴
+        frame.tkraise()  # 해당 Frame을 화면에 표시
+
+# 블록은 현재 스크립트가 직접 실행될 때만 코드가 실행됩니다.
+if __name__ == "__main__":
+# 애플리케이션 인스턴스 생성(MetacognitionTestApp 클래스의 인스턴스를 생성하여 GUI 애플리케이션을 시작합니다.)
+    app = MetacognitionTestApp()
+# 메인 이벤트 루프 시작(root.mainloop()를 호출하여 메인 이벤트 루프를 시작하고, 프로그램이 종료될 때까지 윈도우가 열려 있도록 합니다.)
+    app.mainloop()
---
+++ src/ShowWord.py
@@ -0,0 +1,76 @@
+import tkinter as tk
+import random
+
+# 단어 리스트 100개 일상에서 접하는 단어?
+words_list = [
+    "비행기", "향수", "국회의사당", "병원", "우유",
+    "피타고라스", "까르보나라", "와이파이", "바이러스", "코로나",
+    "바디워시", "부동산", "키보드", "백화점", "고글",
+    "사이다", "로션", "선풍기", "공무원", "휴대폰",
+    "책상", "박람회", "여행", "박스", "지하철",
+    "네덜란드", "시계", "지하철", "친구", "음악",
+    "인스타그램", "영화", "전화", "시간", "공항",
+    "논설", "알람", "애니메이션", "자물쇠", "과제",
+    "네이버", "바닐라", "오토바이", "배달의민족", "관악산",
+    "타이레놀", "삼성페이", "홈런", "거짓말", "예매",
+    "신용카드", "한글", "토스", "소프트웨어", "탄산수",
+    "스테이크", "사랑", "횡단보도", "떡볶이", "에어프라이기"
+    # 더 추가하기
+]
+
+# 단어 리스트에서 20개의 단어를 무작위로 선택
+random_words = random.sample(words_list, 20)
+
+# class App:
+#     def __init__(self):
+#         self.window = Tk()
+#         self.window.title("메타인지 테스트")
+#         window.attributes('-fullscreen',True)
+
+#         self.word_label = Label(self.window, text="20개의 단어를 하나씩 보여드리겠습니다.\n⏰한 단어당 2초\n\n화면에 출력되는 단어를 기억하자!", font=("나눔고딕", 48))
+#         self.word_label.pack(expand=True) # 단어 글자수가 달라서 상대적 위치를 지정해주는 pack 사용
+
+#         self.button_start=Button(self.window,text="START",font=("나눔고딕", 30),command=self.show_word)
+#         self.button_start.pack(expand=True)
+
+#         self.index = 0
+
+#         self.window.mainloop()
+
+#     def show_word(self):
+#         self.button_start.pack_forget()  # 시작 버튼 숨기기
+#         if self.index < 20:
+#             self.word_label.config(text=random_words[self.index])
+#             self.index += 1
+#             self.window.after(100, self.show_word) #빠르게 TEST
+#             #self.window.after(2000, self.show_word)  # 2초 후에 show_word 함수를 다시 호출
+#         else:
+#             self.word_label.config(text="끝!")
+#             self.button_next = Button(self.window, text="NEXT", font=("나눔고딕", 30)) #다음페이지(예상개수입력)로 넘어가는 커맨드추가하면 됨.
+#             self.button_next.pack(expand=True)
+# App()
+
+class ShowWord(tk.Frame):
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+        self.controller = controller
+
+        self.word_label = tk.Label(self, text="20개의 단어를 하나씩 보여드리겠습니다.\n⏰한 단어당 2초\n\n화면에 출력되는 단어를 기억하자!", font=("나눔고딕", 48))
+        self.word_label.pack(expand=True) # 단어 글자수가 달라서 상대적 위치를 지정해주는 pack 사용
+
+        self.button_start= tk.Button(self,text="START",font=("나눔고딕", 30),command=self.show_word)
+        self.button_start.pack(expand=True)
+
+        self.index = 0
+
+    def show_word(self):
+        self.button_start.pack_forget()  # 시작 버튼 숨기기
+        if self.index < 20:
+            self.word_label.config(text=random_words[self.index])
+            self.index += 1
+            self.after(100, self.show_word) #빠르게 TEST
+            #self.window.after(2000, self.show_word)  # 2초 후에 show_word 함수를 다시 호출
+        else:
+            self.word_label.config(text="끝!")
+            self.button_next = tk.Button(self, text="NEXT", font=("나눔고딕", 30)) #다음페이지(예상개수입력)로 넘어가는 커맨드추가하면 됨.
+            self.button_next.pack(expand=True)
