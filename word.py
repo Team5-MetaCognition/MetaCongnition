4861f4dade126e5b0fe88b95b5866106e509e9b2
s5hyeon <s5hyeon_@inha.edu>
2024-06-08 15:40:01 +0900

    fullscreen_word.py

--- word.py
+++ word.py
@@ -25,7 +25,7 @@ class App:
     def __init__(self):
         self.window = Tk()
         self.window.title("메타인지 테스트")
-        self.window.geometry("1280x1024+100+100")
+        window.attributes('-fullscreen',True)
 
         self.word_label = Label(self.window, text="20개의 단어를 하나씩 보여드리겠습니다.\n⏰한 단어당 2초\n\n화면에 출력되는 단어를 기억하자!", font=("나눔고딕", 48))
         self.word_label.pack(expand=True) # 단어 글자수가 달라서 상대적 위치를 지정해주는 pack 사용
