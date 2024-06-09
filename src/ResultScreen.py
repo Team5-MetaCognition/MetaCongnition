8880273149b5e6201fdd2eeb0fc83a3395cdeaec
Yeeun Kim <43701352+y2ny@users.noreply.github.com>
2024-06-10 01:25:00 +0900

    결과 그래프 구현 (#11)
    * Feat: graph 구현
    
    * Fix: 네이밍 수정

--- src/ResultScreen.py
+++ src/ResultScreen.py
@@ -16,7 +16,7 @@ class ResultScreen(tk.Frame):
         result_frame.pack(expand=True)
 
         words_to_display = self.controller.user.random_words #랜덤으로 나온 20개 단어 호출
-        user_input_words = self.controller.user.inputWords #사용자에게 입력 받은 단어 저장
+        user_input_words = self.controller.user.input_words #사용자에게 입력 받은 단어 저장
         
         print("user_input_words:", user_input_words)
         count=0
@@ -33,6 +33,7 @@ class ResultScreen(tk.Frame):
             word_label = tk.Label(result_frame, text=display_text, font=("나눔고딕", 30, "bold"), fg=word_color)
             word_label.grid(row=i//5, column=i%5, padx=20, pady=20)
 
+        self.controller.user.set_matching_word_count(count)
         self.show_report_button = tk.Button(self, text="자세한 결과보기", command=self.show_report,height=3, width=20,font=("나눔고딕", 14))
         self.show_report_button.pack(pady=10)    
         
